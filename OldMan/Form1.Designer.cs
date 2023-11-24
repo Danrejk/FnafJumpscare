8cddfb66b53e7ee0f0efde37d09a7cbeb98ca15c
Danrejk <SDanrejk@gmail.com>
2023-11-24 19:49:46 +0100

    Added Old Man Consequences

---
+++ OldMan/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace OldMan
+{
+    partial class Gimp
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gimp));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(248, 214);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(37, 18);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Gimp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(480, 270);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Gimp";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ OldMan/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace OldMan
+{
+    public partial class Gimp : Form
+    {
+        //variables you can adjust
+        public int speed = 10; //how much pixels fish travels in 0.025 seconds
+        public int timeLimit = 200; //time limit (40 = 1 second)
+
+        //DO NOT MODIFY variables needed for correct work of program 
+        bool click = false;
+        int timer = 0;
+        public Gimp()
+        {
+            InitializeComponent();
+            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Ruszanie(true);
+            //thread cos SoundPlayer.PlayLoop does not have delay
+            var thread = new Thread(LoopClick) { };
+            thread.Start();
+        }
+
+        public async void Ruszanie(bool goesRight)
+        {
+            //i basicly copied same code twice with difference beeing incrementation/decramentation and image (could be more efficient)
+            if (goesRight)
+            {
+                pictureBox1.Image = Properties.Resources._167;
+                for (int i = 100; i < 350; i = i + 10)
+                {
+
+                    pictureBox1.Location = new Point(i, 215);
+                    await Task.Delay(25);
+                    //checking of player click (could be more efficient)
+                    if (click == true)
+                    {
+                        if (pictureBox1.Location.X > 200 && pictureBox1.Location.X < 250)
+                        {
+                            new SoundPlayer(Properties.Resources.fish4).PlaySync();
+                            click = false;
+                            Application.Exit();
+
+                        }
+                        else
+                        {
+                            playerMissed();
+                            click = false;
+                        }
+                    }
+                    timer++;
+
+                }
+                if (timer > timeLimit)
+                {
+                    click = true;
+                    playerMissed();
+                    click = false;
+                }
+                Ruszanie(false);
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources._164;
+                for (int i = 350; i > 100; i = i - 10)
+                {
+                    pictureBox1.Location = new Point(i, 215);
+                    await Task.Delay(25);
+                    if (click == true)
+                    {
+                        //the same as above
+                        if (pictureBox1.Location.X > 200 && pictureBox1.Location.X < 250)
+                        {
+                            new SoundPlayer(Properties.Resources.fish4).PlaySync();
+                            click = false;
+                            Application.Exit();
+
+                        }
+                        else
+                        {
+                            playerMissed();
+                            click = false;
+                        }
+                    }
+                    timer++;
+                }
+                if (timer > timeLimit)
+                {
+                    click = true;
+                    playerMissed();
+                    click = false;
+                }
+                Ruszanie(true);
+            }
+
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals('c'))
+            {
+                //yes I use global variable, if true it ends the game and determisnes if player win or lose
+                click = true;
+            }
+        }
+
+        private void LoopClick()
+        {
+            while (!click)
+            {
+                new SoundPlayer(Properties.Resources.fish1).Play();
+                Thread.Sleep(250);
+            }
+        }
+
+        public void playerMissed()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                new SoundPlayer(Properties.Resources.fish3).PlaySync();
+            }
+            Application.Exit();
+            //insert function that will activate after player loosing
+        }
+    }
+}
